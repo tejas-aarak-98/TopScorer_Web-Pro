4784972e7d8d87c6436c0997e7037be02c17142d
tejas-aarak-98 <tejasaarak1@gmail.com>
2026-04-10 18:05:53 +0530

    change login api

--- src/component/Login/login.jsx
+++ src/component/Login/login.jsx
@@ -33,7 +33,7 @@ const Login = () => {
         e.preventDefault();
 
 
-        const Api = "http://localhost:8080/topscorer/login";
+        const Api = "https://topscorerbackend-4.onrender.com/topscorer/login";
 
         const userDetails = {
 
